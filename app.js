da70910ec4fe3d3b6f65626a945efc29a5018615
2potatoes <nobody@nowhere.duh>
2013-09-27 20:50:32 -0700

    Basic json conversion working

--- app.js
+++ app.js
@@ -45,14 +45,44 @@ $(function(){
     });
 
     $('#inputJSON').change(function(){
-        // console.log("JSON change");
-        // console.log(this.value);
         var jsonToRun = $.parseJSON(this.value);
+        var newNutrition = convertJSONNutritionToGeneticNutrition(jsonToRun.nutrientTargets);
         testGeneticSoylent = new GeneticSoylent({
-                    ingredients: jsonToRun.ingredients,
-                    targetNutrients: jsonToRun.nutrientTargets
+                    ingredients: convertJSONIngredientsToGeneticIngredients(jsonToRun.ingredients),
+                    targetNutrients: newNutrition
         });
+        testGeneticSoylent.reset();
+        testGeneticSoylent.render();
     });
+
+    function convertJSONIngredientsToGeneticIngredients(ingredients){
+        $.each(ingredients, function(key, value){
+            ingredients[key]["maxAmount"] = 500;
+        });
+        return ingredients;
+    }
+
+    function convertJSONNutritionToGeneticNutrition(nutrition){
+        var newNutrition = [];
+        $.each(nutrition, function(key, value){
+            var trueKey = key.replace("_max","");
+            if (!newNutrition[trueKey]){
+                newNutrition[trueKey] = {};
+            }
+
+            if (key.indexOf("_max") >= 0){
+                newNutrition[trueKey.replace("_max","")]["max"] = value;
+            }
+            else {
+                newNutrition[trueKey]["min"] = value;
+                newNutrition[trueKey]["importanceFactor"] = 1;
+                if (!newNutrition[trueKey]["max"]){
+                    newNutrition[trueKey]["max"] = newNutrition[trueKey]["min"] * 10;
+                }
+            }
+        });
+        return newNutrition;
+    }
 
     testGeneticSoylent.render();
 });
