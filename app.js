6e32752aee45ca12eb25a56f7061c303bd219684
2potatoes <nobody@nowhere.duh>
2013-09-27 21:33:42 -0700

    Normalize nutrient data, add comment

--- app.js
+++ app.js
@@ -46,17 +46,25 @@ $(function(){
 
     $('#inputJSON').change(function(){
         var jsonToRun = $.parseJSON(this.value);
-        var newNutrition = convertJSONNutritionToGeneticNutrition(jsonToRun.nutrientTargets);
         testGeneticSoylent = new GeneticSoylent({
                     ingredients: convertJSONIngredientsToGeneticIngredients(jsonToRun.ingredients),
-                    targetNutrients: newNutrition
+                    targetNutrients: convertJSONNutritionToGeneticNutrition(jsonToRun.nutrientTargets)
         });
+
         testGeneticSoylent.reset();
         testGeneticSoylent.render();
     });
 
     function convertJSONIngredientsToGeneticIngredients(ingredients){
         $.each(ingredients, function(key, value){
+            // divide each value by serving size to normalize them
+            var servingSize = ingredients[key]["serving"];
+            $.each(value, function(ingredientNutrient, ingredientValue){
+                if (Number(ingredients[key][ingredientNutrient])) {
+                   ingredients[key][ingredientNutrient] = ingredientValue/servingSize;
+               }
+            });
+            // also, for each ingredient, add a maxAmount
             ingredients[key]["maxAmount"] = 500;
         });
         return ingredients;
@@ -65,22 +73,34 @@ $(function(){
     function convertJSONNutritionToGeneticNutrition(nutrition){
         var newNutrition = [];
         $.each(nutrition, function(key, value){
+            // get the name of the trueKey, which doesn't include _max
+            // later, we will put the value of _max into the key max of the nutrient trueKey
             var trueKey = key.replace("_max","");
+
+            // if the object for this nutrient doesn't exist yet, add it
             if (!newNutrition[trueKey]){
                 newNutrition[trueKey] = {};
             }
 
+            // if this is a _max nutrition key, add it as the max value to trueKey
             if (key.indexOf("_max") >= 0){
                 newNutrition[trueKey.replace("_max","")]["max"] = value;
             }
+
+            // otherwise take this value as the min and set the default importanceFactor
             else {
                 newNutrition[trueKey]["min"] = value;
                 newNutrition[trueKey]["importanceFactor"] = 1;
+                // we'll need to add a max for all values, so
+                // if it's othrewise undefined, we choose min*10
                 if (!newNutrition[trueKey]["max"]){
                     newNutrition[trueKey]["max"] = newNutrition[trueKey]["min"] * 10;
                 }
             }
         });
+
+        // we delete "name", since it's not a nutrient
+        delete newNutrition["name"];
         return newNutrition;
     }
 
